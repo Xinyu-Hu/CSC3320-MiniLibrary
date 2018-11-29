e5a36f3c159a9f2417a55fe432308cc707934103
Xinyu-Hu <31017838+Xinyu-Hu@users.noreply.github.com>
2018-11-28 21:07:04 -0500

    remove comments

--- miniLibrary.c
+++ miniLibrary.c
@@ -1,6 +1,5 @@
 
 #include "pch.h"
-//#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +7,6 @@
 
 const char userTxt[MAX] = "./UserInfo.txt";
 const char bookTxt[MAX] = "./MyLibrary.txt";
-//static int userID = 0, userNum = 0;
 
 typedef struct {
 	int year;
@@ -62,44 +60,6 @@ User* findUser(int fieldNum, char* value) {
 
 //return user* containing userID, accountName, password info if user is found.
 User* findUserByName(char myAccountName[], char myPsw[]) {
-	////printf("%s",path);
-	//char buffer[MAX];
-	//FILE *userInfo = fopen(userTxt, "r+");
-	//if (!userInfo) {
-	//	printf("Failed to open User.txt\n");
-	//	exit(1);
-	//}
-	//while (!feof(userInfo)) {
-	//	fgets(buffer, MAX, userInfo); // gets profile of user in buffer as char[]
-	//	
-	//	char *account, *psw, *userType;
-	//	//Gets account info in account and compare.
-	//	account = _strdup(buffer);
-	//	account = strtok(account, ",");
-	//	for (int i = 0; i < 3; i++) {
-	//		account = strtok(NULL, ",");
-	//	}
-	//	//If account matched, compare password.
-	//	if (strcmp(account, myAccountName) == 0) {
-	//		psw = strtok(NULL, ",");
-	//		if (strcmp(psw, myPsw) == 0) {
-	//			printf("Log in successfully.\n");
-	//			userType = strtok(NULL, "\n");
-	//			User buffUser;
-	//			initUser(&buffUser, account, psw);
-	//			if (strcmp(userType, "B") == 0) {
-	//				printf("You are borrower.\n");
-	//			}
-	//			else if (strcmp(userType, "L") == 0) {
-	//				printf("You are librarian.\n");
-	//			}
-	//			return EXIT_SUCCESS;
-	//		}
-	//	}	
-	//}
-	//fclose(userInfo);
-	//printf("Log in failed\n");
-	//return EXIT_SUCCESS;
 	if (findUser(4, myAccountName) == NULL) {
 		printf("Log in failed\n");
 		return NULL;
